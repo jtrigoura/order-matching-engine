fb641ae98bd142e63f6c44b1c57da79220dc8c7e
Jorge <jorgetrigoura1@gmail.com>
2026-01-05 20:48:37 -0500

    Milestone 0: OrderBook skeleton with bids/asks

---
+++ build/CMakeFiles/order_matching_engine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/order_matching_engine.dir/src/main.cpp.o"
+  "CMakeFiles/order_matching_engine.dir/src/main.cpp.o.d"
+  "CMakeFiles/order_matching_engine.dir/src/order_book.cpp.o"
+  "CMakeFiles/order_matching_engine.dir/src/order_book.cpp.o.d"
+  "order_matching_engine"
+  "order_matching_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/order_matching_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
