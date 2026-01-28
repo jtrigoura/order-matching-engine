27d2cb90808bcedbb9e4f1c2aa2d5a3d965cdc6f
Jorge <jorgetrigoura1@gmail.com>
2026-01-27 21:24:14 -0500

    Milestone 3.1: test cancel event in engine

--- tests/engine_tests.cpp
+++ tests/engine_tests.cpp
@@ -21,7 +21,19 @@ TEST(EngineTests, ProcessesEventsAndMatches) {
 
     engine.stop();
 
-    // Book trades are recorded inside OrderBook
     ASSERT_EQ(engine.book().trades().size(), 1u);
     EXPECT_EQ(engine.book().trades()[0].quantity, 7u);
+}
+
+TEST(EngineTests, CancelEventPreventsTrade) {
+    Engine engine;
+    engine.start();
+
+    engine.submit(NewOrder{Order{1, Side::Buy, 10050, 10}});
+    engine.submit(CancelOrder{1});
+    engine.submit(NewOrder{Order{2, Side::Sell, 10050, 10}});
+
+    engine.stop();
+
+    ASSERT_TRUE(engine.book().trades().empty());
 }
